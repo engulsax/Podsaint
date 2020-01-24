e076464f26acf26fc018c12fefb3a1538b947452
Adam Leo <adamcarlleo@gamil.com>
2020-01-24 15:32:53 +0100

    added views folder with layout aswell as installed npm package express-handlebars

---
+++ web-application/src/pl/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
+const app = express()
+
+app.set("views", "src/pl/views")
+
+app.engine('hbs', expressHandlebars({
+  extname: ".hbs",
+  defaultLayout: "layout",
+}))
+
+app.get('/', function(request, response){
+  response.render("home.hbs")
+})
+
+app.listen(8080, function(){
+  console.log("Web application listening on port 8080.")
+})
