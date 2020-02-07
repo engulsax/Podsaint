3eef94a3d344d4b575c72d26ea9a65c84c209ad7
Adam Leo <adamcarlleo@gamil.com>
2020-02-07 17:02:21 +0100

    using util to make use of promises in database instead of callbacks

--- web-application/src/dal/reviews-repository.js
+++ web-application/src/dal/reviews-repository.js
@@ -1,41 +1,49 @@
 
 const mySql = require("mysql")
+const util = require('util')
 
-const database = mySql.createConnection({
+const conn = mySql.createConnection({
 	host: "database",
 	user: "root",
 	password: "theRootPassword",
 	database: "webAppDatabase"
 })
 
+const db = util.promisify(conn.query).bind(conn)
 
 
 
-
-exports.newPodcastReview = async function newPodcastReview(collectionId,humorRating,factRating,productionQualty,overallRating,reviewTest,seriousnessRating){
+exports.newPodcastReview = async function newPodcastReview(collectionId, humorRating, factRating, productionQualty, overallRating, reviewTest, seriousnessRating) {
 
 	const userId = 1
 	const query = "INSERT INTO reviews (user_id, pod_id, production_quality_rating, seriousness_rating, humor_rating, fact_rating, overall_rating, review_text) VALUES(?, ?, ?, ?, ?, ?, ?, ?)"
-	const values = [userId,collectionId,productionQualty,seriousnessRating,humorRating,factRating,overallRating,reviewTest]
-	
+	const values = [userId, collectionId, productionQualty, seriousnessRating, humorRating, factRating, overallRating, reviewTest]
+
 	console.log("kommit hit reviews-repository")
-	
+
 	try {
-		const podcast =  await this.getPodcastById(collectionId)
-		
+		const podcast = await this.getPodcastById(collectionId)
+
 		console.log("------------------------podcast----------------")
 		console.log(podcast)
-		if(podcast===undefined){
+		if (podcast == undefined) {
 			console.log("ska inte komma hit ska inte komma hit")
 			await this.addPodcast(collectionId)
 		}
 
-	}catch {
+	} catch {
 		console.log("Database error")
 		//throw error
 	}
-	
-	
+
+	try {
+		return await db(query, values)
+
+	} catch (error) {
+		console.log("error when returning in newPodcastReview")
+	}
+
+	/*
 	await database.query(query,values, function(error,result){
 		
 		if(error){
@@ -43,16 +51,29 @@ exports.newPodcastReview = async function newPodcastReview(collectionId,humorRat
 			throw error
 		}
 		return result
-	})
-	
+	})*/
+
 }
 
-exports.getPodcastById = async function getPodcastById(collectionId){
+exports.getPodcastById = async function getPodcastById(collectionId) {
 
 	console.log("kommit hit get podcastbyid")
 
 	const query = "SELECT * FROM podcasts WHERE pod_id = ?"
 	const values = [collectionId]
+	try {
+		const response = await db(query, values)
+		console.log("---------------------result-------------")
+		console.log(response)
+		console.log(response[0].pod_id)
+		return response[0].pod_id
+
+	} catch (error) {
+		// console.log(response)
+		console.log("error in getPodcastById");
+	}
+
+	/*
 	database.query(query,values, async function(error, result){
 		
 		if(error){
@@ -67,20 +88,28 @@ exports.getPodcastById = async function getPodcastById(collectionId){
 		}
 		
 		
-	})
+	})*/
 
 }
-exports.addPodcast = async function addPodcast(collectionId){
+exports.addPodcast = async function addPodcast(collectionId) {
 	console.log("Kommit hit add podcast")
 	const query = "INSERT INTO podcasts(pod_id) VALUES(?)"
 	const values = [collectionId]
 
-	database.query(query, values, function(error, result){
+	/*database.query(query, values, function(error, result){
 		if(error){
 			throw error
 		}
 		console.log(result)
-	})
+	})*/
+
+	try {
+		const response = await db(query, values)
+		return response
+	} catch (error) {
+		console.log("error in addPodcast")
+	}
+
 }
 /*
 CREATE TABLE reviews (
